69ae2cae0e9fe76a5d8c8f26ed26006d25d7d6b7
byorck <work-byorck@yandex.ru>
2025-09-02 22:33:52 +0300

    Initial commit

---
+++ src/main/java/com/starbank/StarProductAdvisor/StarProductAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.starbank.StarProductAdvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarProductAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarProductAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starbank/StarProductAdvisor/StarProductAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbank.StarProductAdvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarProductAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
