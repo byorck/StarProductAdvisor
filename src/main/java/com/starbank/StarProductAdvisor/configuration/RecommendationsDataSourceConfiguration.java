700df37d6d21077762ebbfdba6b2d15ca606429e
byorck <work-byorck@yandex.ru>
2025-09-10 17:00:22 +0300

    Добавил зависимости для работы с БД для динамических правил. Модернизировал файл настроек. Настроил миграции. Написал changeset создающие три таблицы для работы с динамическими правилами

--- src/main/java/com/starbank/StarProductAdvisor/configuration/RecommendationsDataSourceConfiguration.java
+++ src/main/java/com/starbank/StarProductAdvisor/configuration/RecommendationsDataSourceConfiguration.java
@@ -3,8 +3,10 @@ package com.starbank.StarProductAdvisor.configuration;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
@@ -14,15 +16,21 @@ public class RecommendationsDataSourceConfiguration {
 
     @Bean(name = "recommendationsDataSource")
     public DataSource recommendationsDataSource(@Value("${application.recommendations-db.url}") String recommendationsUrl) {
-        var dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(recommendationsUrl);
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setReadOnly(true);
-        return dataSource;
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(recommendationsUrl);
+        ds.setDriverClassName("org.h2.Driver");
+        ds.setReadOnly(true); // база статическая, запрещаем запись
+        return ds;
     }
 
     @Bean(name = "recommendationsJdbcTemplate")
-    public JdbcTemplate recommendationsJdbcTemplate(@Qualifier("recommendationsDataSource") DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
+    public JdbcTemplate recommendationsJdbcTemplate(@Qualifier("recommendationsDataSource") DataSource ds) {
+        return new JdbcTemplate(ds);
+    }
+
+    @Primary
+    @Bean(name = "defaultDataSource")
+    public DataSource defaultDataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().build();
     }
 }
