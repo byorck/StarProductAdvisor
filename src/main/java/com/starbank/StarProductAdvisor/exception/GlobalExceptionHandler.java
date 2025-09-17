4513e9d8edb04071003a3adbb37311a4f11d7fa5
Roman <filinroman79@gmail.com>
2025-09-18 00:55:34 +0300

    Добавлен метод MethodArgumentNotValidException в класс GlobalExceptionHandler. Добавлены тесты. Добавлены аннотации в DTO.

--- src/main/java/com/starbank/StarProductAdvisor/exception/GlobalExceptionHandler.java
+++ src/main/java/com/starbank/StarProductAdvisor/exception/GlobalExceptionHandler.java
@@ -2,13 +2,29 @@ package com.starbank.StarProductAdvisor.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends RuntimeException {
+
+    //валидация входных данных, перехватывает исключения, возвращает статус 400.
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("error", "Validation Failed", "details", errors));
+    }
+
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Object> handleUserNotFound(UserNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
